cdf5c9672a5c258851a3b0bc89bee60821570a7e
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-08-19 14:05:08 +0200

    Created main.c

---
+++ ELOB Board ATmega2560/ELOB Board ATmega2560/main.c
@@ -0,0 +1,18 @@
+/*
+ * ELOB Board ATmega2560.c
+ *
+ * Created: 19.08.2021 12:55:49
+ * Author : Puncher1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* INITIALIZATION */
+    while (1) 
+    {
+    }
+}
+
